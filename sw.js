a48ffe5f3346c96f35a4e6041fa0bcf06fa882e3
cutepms123-blip <cutepms123@gmail.com>
2026-08-23 02:20:01 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,9 @@
-const VERSION='team-eysl-final-20260823';
+const VERSION='team-eysl-final-mobile-nav-20260823';
 self.addEventListener('install',()=>self.skipWaiting());
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('activate',e=>e.waitUntil(
+  caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==VERSION).map(k=>caches.delete(k))))
+  .then(()=>self.clients.claim())
+));
 self.addEventListener('fetch',e=>{
   if(e.request.method!=='GET') return;
   e.respondWith(fetch(e.request,{cache:'no-store'}).catch(()=>caches.match(e.request)));
