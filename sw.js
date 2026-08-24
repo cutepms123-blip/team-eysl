6b67db81f88cc1aa402d3feaf3eb8506a6027635
cutepms123-blip <cutepms123@gmail.com>
2026-08-24 19:11:35 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION='team-eysl-final49-filters-race-autologin-20260824';
+const VERSION='team-eysl-final50-filters-permissions-events';
 self.addEventListener('install',()=>self.skipWaiting());
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request,{cache:'no-store'}).catch(()=>caches.match(e.request)))});
