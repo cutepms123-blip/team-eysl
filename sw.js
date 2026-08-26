de4028b22e1144fa378944a7a1fe3d51bdfbccee
cutepms123-blip <cutepms123@gmail.com>
2026-08-26 22:10:37 +0900

    Deploy notice timestamp fix v94

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const VERSION='team-eysl-final93-owner-time';
+const VERSION='team-eysl-final94-notice-time';
 const CACHE=`team-eysl-${VERSION}`;
-const PRECACHE=['/manifest.webmanifest','/icon-192.png','/icon-512.png','/apple-touch-icon.png','/enhancements-v93.js'];
+const PRECACHE=['/manifest.webmanifest','/icon-192.png','/icon-512.png','/apple-touch-icon.png','/enhancements-v93.js','/timestamp-v94.js'];
 
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(PRECACHE)).then(()=>self.skipWaiting()));
@@ -13,7 +13,8 @@ async function navigationResponse(req){
     const res=await fetch(req,{cache:'no-store'});
     if(!res.ok)return res;
     let html=await res.text();
-    if(!html.includes('/enhancements-v93.js'))html=html.replace('</body>','<script src="/enhancements-v93.js?v=final93-owner-time"></script></body>');
+    if(!html.includes('/enhancements-v93.js'))html=html.replace('</body>','<script src="/enhancements-v93.js?v=final94"></script></body>');
+    if(!html.includes('/timestamp-v94.js'))html=html.replace('</body>','<script src="/timestamp-v94.js?v=final94"></script></body>');
     return new Response(html,{status:res.status,statusText:res.statusText,headers:new Headers(res.headers)});
   }catch(_){
     const cached=await caches.match('/index.html');
