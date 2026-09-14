8a793e136ad98d24441081c3310bca36334091e2
cutepms123-blip <cutepms123@gmail.com>
2026-09-14 15:06:19 +0900

    Refresh PWA cache for member relay records v193

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION='team-eysl-final192-race-range-relays';
+const VERSION='team-eysl-final193-member-relay';
 const CACHE=`team-eysl-${VERSION}`;
 
 const PRECACHE=[
@@ -45,7 +45,7 @@ async function navigationResponse(req){
         new RegExp(`<script[^>]+src=["'][^"']*/${escaped}[^"']*["'][^>]*><\\/script>`,'g'),
         ''
       );
-      html=html.replace('</body>',`<script src="/${file}?v=final192"></script></body>`);
+      html=html.replace('</body>',`<script src="/${file}?v=final193"></script></body>`);
     }
 
     const headers=new Headers(res.headers);
