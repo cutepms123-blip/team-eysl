7791c58bede2d96fd68addb7f2bcd6f7c9fdc895
cutepms123-blip <cutepms123@gmail.com>
2026-09-14 13:33:37 +0900

    Refresh PWA cache after home load stability fix v189

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION='team-eysl-final188-event-icon-dedupe';
+const VERSION='team-eysl-final189-home-load-stability';
 const CACHE=`team-eysl-${VERSION}`;
 
 const PRECACHE=[
@@ -45,7 +45,7 @@ async function navigationResponse(req){
         new RegExp(`<script[^>]+src=["'][^"']*/${escaped}[^"']*["'][^>]*><\\/script>`,'g'),
         ''
       );
-      html=html.replace('</body>',`<script src="/${file}?v=final188"></script></body>`);
+      html=html.replace('</body>',`<script src="/${file}?v=final189"></script></body>`);
     }
 
     const headers=new Headers(res.headers);
